92bf63e0c031b3b3791436e33ce02d01f4b279cd
kkarimwahba <karimwahba53@gmail.com>
2024-04-07 01:21:52 +0200

    error handling

--- lib/services/firebase_auth.dart
+++ lib/services/firebase_auth.dart
@@ -5,17 +5,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final _auth = FirebaseAuth.instance;
-  // Future<User?> createUserWithEmalAndPassword(
-  //     String email, String password) async {
-  //   try {
-  //     final cred = await _auth.createUserWithEmailAndPassword(
-  //         email: email, password: password);
-  //     return cred.user;
-  //   } catch (e) {
-  //     print(e.toString());
-  //   }
-  //   return null;
-  // }
 
   Future<User?> signinwithemailandpassword(
       String email, String password) async {
